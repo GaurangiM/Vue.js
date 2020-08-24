fa0acc0f4d1fbd0927bcd94f207859cc04249bb8
Gaurangi <manegaurangi89@gmail.com>
2020-08-24 15:42:01 +0200

    Add project artifacts

---
+++ vue-stocktrading/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Trade or view your Portfolio</h1>
+        <h6>You may save or load your data</h6>
+        <h6>Click on 'End Day' to begin a new day !</h6>
+        <hr>
+        <h6>Your funds: {{ funds | currency }}</h6>
+    </div>
+    
+</template>
+<script>
+export default {
+    computed: {
+        funds() {
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
---
+++ vue-stocktrading/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, name: 'BMW', price: '201'},
+    {id: 2, name: 'Twitter', price: '120'},
+    {id: 3, name: 'Facebook', price: '85'},
+    {id: 4, name: 'KPN', price: '20'},
+]
